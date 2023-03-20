26c33b4f5652e75f5a48a2be09ef6148221d6896
Skrypnikaw <skrypnikaw@gmail.com>
2023-03-20 22:54:25 +0300

    Добавьте файлы проекта.

---
+++ NWEncyclopedia/Model/MenuModel.cs
@@ -0,0 +1,16 @@
+﻿using NWEncyclopedia.ViewModel.Base;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NWEncyclopedia.Model
+{
+    internal class MenuModel : ViewModelBase
+    {
+       
+    }
+}
---
+++ NWEncyclopedia/ViewModel/MenuViewModel.cs
@@ -0,0 +1,44 @@
+﻿using NWEncyclopedia.Model;
+using NWEncyclopedia.View.Pages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Controls;
+using System.Threading.Tasks;
+using NWEncyclopedia.ViewModel.Base;
+
+namespace NWEncyclopedia.ViewModel
+{
+    internal class MenuViewModel : ViewModelBase
+    {
+        private string _titleMenu;
+
+        private List<string> _menuFields;
+
+        public string TitleMenu
+        {
+            get => _titleMenu;
+            set => Set(ref _titleMenu, value);
+        }
+
+        public List<string> MenuFields
+        {
+            get { return _menuFields; }
+            set { _menuFields = value; }
+        }
+        public MenuViewModel()
+        {
+            _titleMenu = "Меню";
+            _menuFields = new List<string>
+            {
+                "Правила Группы",
+                "Каталог",
+                "Гайды",
+                "Таблицы",
+                "Серверы",
+                "Калькулятор"
+            };
+        }
+    }
+}
